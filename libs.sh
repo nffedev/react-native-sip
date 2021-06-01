380ea546b38746235e8c840e9b7afc9f2aaf1e70
nffedev <59941291+nffedev@users.noreply.github.com>
2021-06-01 12:15:04 -0700

    Update libs.sh

--- libs.sh
+++ libs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-VERSION="v2.9.0"
-URL="https://github.com/nffedev/react-native-pjsip-builder/archive/refs/tags/v2.9.0.tar.gz"
+VERSION="v2.9.1"
+URL="https://github.com/nffedev/react-native-pjsip-builder/archive/refs/tags/v2.9.1.tar.gz"
 LOCK=".libs.lock"
 DEST=".libs.tar.gz"
 DOWNLOAD=true
@@ -30,7 +30,7 @@ if [ "$DOWNLOAD" = true ]; then
     rm -f "${DEST}"
 
     echo "${VERSION}" > ${LOCK}
-    cd react-native-pjsip-builder-2.9.0
+    cd react-native-pjsip-builder-2.9.1
     ./release.sh
     cp -rf dist/ios/VialerPJSIP.framework ../ios/
     cp -rf dist/android/src/* ../android/src
