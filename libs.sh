6faef35985af4488278547454dc38369897417ef
nffedev <59941291+nffedev@users.noreply.github.com>
2021-06-01 11:07:56 -0700

    Update libs.sh

--- libs.sh
+++ libs.sh
@@ -2,7 +2,7 @@
 set -e
 
 VERSION="v2.9.0"
-URL="https://github.com/nffedev/react-native-pjsip-builder/archive/v2.9.0.tar.gz"
+URL="https://github.com/nffedev/react-native-pjsip-builder/archive/refs/tags/v2.9.0.tar.gz"
 LOCK=".libs.lock"
 DEST=".libs.tar.gz"
 DOWNLOAD=true
